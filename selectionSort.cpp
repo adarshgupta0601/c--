e5473e51d92b32e7d3a7f446a134160604db1b82
adarshgupta0601 <adarshg.work@gmail.com>
2022-09-08 10:20:55 +0530

    added new file

---
+++ selectionSort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int sortedArray(int arr[],int size){
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+}
+
+int selectionSort(int arr[],int size){
+    for(int i = 0; i<size-1; i++){
+        int minIndex = i;
+
+        for(int j = i+1; j<size; j++){
+            if(arr[minIndex]>arr[j])
+                minIndex = j;
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+    
+    sortedArray(arr,size);
+}
+
+int main(){
+    int num[6]={23,45,21,34,67,32};
+
+    selectionSort(num,6);
+}
