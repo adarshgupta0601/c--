8e110abe6986e6913f6cf6cccab26d76fe79a945
adarshgupta0601 <adarshg.work@gmail.com>
2022-09-09 02:07:57 +0530

    added new file

--- pattern.cpp
+++ pattern.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main(){
+    int n=5;
     int row=5, col=6;
     // cout<<"Eneter the now of Rows and Column: ";
     // cin>>row,col;
     
 
-    while(row<=col){
+    while(row<=n){
         col = 1;
-        while(row<col){
+        while(col<=row){
             cout<<col;
             col+=1;
         }
         row+=1;
-        cout<<endl;
+        //cout<<"hi"<<endl;
     }
 }
